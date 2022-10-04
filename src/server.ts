f3f72c98a42b1d02047c1d86396f547a5cc4761d
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-10-04 11:13:11 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+
+import path from 'node:path';
+
+import { ApolloServer } from 'apollo-server';
+import { buildSchema } from 'type-graphql';
+
+import { AppointmentsResolver } from './resolvers/appointments-resolver';
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [AppointmentsResolver],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await server.listen();
+
+  console.log(`🚀 HTTP server running on ${url}`);
+}
+
+bootstrap();
